92c850368319d48cc5dc017cb89aa481c4a1d570
asari <asari.mtr@gmail.com>
2012-07-12 22:38:29 +0900

    init

---
+++ src/com/limelabo/wausau/WausauActivity.java
@@ -0,0 +1,13 @@
+package com.limelabo.wausau;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class WausauActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
